dd7c634bda86108b562aff15b44f71c3edc66cb9
tuankiet3 <vohtuankiet@dtu.edu.vn>
2024-04-19 20:03:16 +0700

    addModeTheme

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,51 @@
-import Button from "@mui/material/Button";
-import HomeIcon from "@mui/icons-material/Home";
-import { pink } from "@mui/material/colors";
+import { useColorScheme } from "@mui/material/styles";
+import { FormControl, InputLabel, MenuItem, Select, Box } from "@mui/material";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import SettingsSystemDaydreamIcon from "@mui/icons-material/SettingsSystemDaydream";
 function App() {
+  function SelectMode() {
+    const handleChange = (event) => {
+      const selectedMode = event.target.value;
+      setMode(selectedMode);
+    };
+    const { mode, setMode } = useColorScheme(); // save theme to local storage
+
+    return (
+      <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+        <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
+        <Select
+          labelId="label-select-dark-light-mode"
+          id="label-select-dark-light-mode"
+          value={mode}
+          label="Mode"
+          onChange={handleChange}
+        >
+          <MenuItem value="light">
+            <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+              <LightModeIcon />
+              Light
+            </Box>
+          </MenuItem>
+          <MenuItem value="dark">
+            <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+              <DarkModeIcon />
+              Dark
+            </Box>
+          </MenuItem>
+          <MenuItem value="system">
+            <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+              <SettingsSystemDaydreamIcon />
+              System
+            </Box>
+          </MenuItem>
+        </Select>
+      </FormControl>
+    );
+  }
   return (
     <>
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-      <HomeIcon />
-      <HomeIcon color="primary" />
-      <HomeIcon color="secondary" />
-      <HomeIcon color="success" />
-      <HomeIcon color="action" />
-      <HomeIcon color="disabled" />
-      <HomeIcon sx={{ color: pink[500] }} />
+      <SelectMode />
     </>
   );
 }
---
+++ src/theme.jsx
@@ -0,0 +1,23 @@
+import { cyan, deepOrange, orange, teal } from "@mui/material/colors";
+import { experimental_extendTheme as extendTheme } from "@mui/material/styles";
+
+// A custom theme for this app
+const theme = extendTheme({
+  colorSchemes: {
+    light: {
+      palette: {
+        primary: teal,
+        secondary: deepOrange,
+      },
+    },
+    dark: {
+      palette: {
+        primary: cyan,
+        secondary: orange,
+      },
+    },
+  },
+  // ...other properties
+});
+
+export default theme;
